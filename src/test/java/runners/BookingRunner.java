f7dfc359bafe8fbdc905857709b1bcd2c1a6b515
Roman Iliuchik <r.iliuchik@gmail.com>
2020-05-30 11:19:33 +0300

    Adding cucumber scenarios

--- src/test/java/runners/BookingRunner.java
+++ src/test/java/runners/BookingRunner.java
@@ -2,9 +2,9 @@ package runners;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import tests.booking.BookingParisTest;
-import tests.booking.BookingMoscowTest;
-import tests.booking.BookingOsloTest;
+import tests.booking.tripParis.BookingParisTest;
+import tests.booking.tripMoscow.BookingMoscowTest;
+import tests.booking.tripOslo.BookingOsloTest;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({BookingParisTest.class, BookingMoscowTest.class, BookingOsloTest.class})
